c9fe686c6d34ebeb6f6902445a8b36381e30b43a
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-03 00:50:55 +0800

    Initial setup.py
    Required for experimenting with vim plugin.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="npyrepl",
+    version="0.1.0",
+    author="Andrey Krainyak",
+    description="Python REPL",
+    url="https://github.com/mode89/npyrepl",
+    packages=setuptools.find_packages(),
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
