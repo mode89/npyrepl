b51718e49762b46d3a6791ed97d8d2e7905ff787
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-02 18:58:53 +0800

    Multi-line input in terminal client
    Enable entering functions and multiline statements.

--- prepl/terminal.py
+++ prepl/terminal.py
@@ -6,6 +6,21 @@ def read_port_file():
     with open(".prepl-port", "r") as port_file:
         return int(port_file.read())
 
+def read_command():
+    lines = []
+    while True:
+        prompt = "> " if not lines else ". "
+        line = input(prompt).rstrip()
+        if not line:
+            if lines:
+                break
+            else:
+                continue
+        lines.append(line)
+        if line[-1] != ":" and len(lines) == 1:
+            break
+    return "\n".join(lines)
+
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(("localhost", read_port_file()))
@@ -13,16 +28,15 @@ if __name__ == "__main__":
         rfile = sock.makefile("rb")
 
         while True:
-            command = input("> ").strip()
-            if command:
-                write_packet(wfile, {
-                    "op": "eval",
-                    "code": command,
-                })
-                wfile.flush()
-                result = read_packet(rfile)
-                ex = result.get("ex", None)
-                if ex is None:
-                    print(result["value"])
-                else:
-                    print(ex)
+            command = read_command()
+            write_packet(wfile, {
+                "op": "eval",
+                "code": command,
+            })
+            wfile.flush()
+            result = read_packet(rfile)
+            ex = result.get("ex", None)
+            if ex is None:
+                print(result["value"])
+            else:
+                print(ex)
