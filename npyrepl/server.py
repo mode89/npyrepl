db943de3977d8c29851ffb776d1232833fcdd880
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-03 17:57:05 +0800

    Refactor exception handling of requests
    Simplify server code.

--- npyrepl/server.py
+++ npyrepl/server.py
@@ -22,14 +22,11 @@ def run():
                 if request is None:
                     break
 
-                if request.op == "eval":
-                    value, ex = evaluate(request.code, vars(namespace))
-                    if ex is None:
+                try:
+                    if request.op == "eval":
+                        value = _evaluate(request.code, vars(namespace))
                         write_packet(self.wfile, SN(value=str(value)))
-                    else:
-                        write_packet(self.wfile, SN(ex=str(ex)))
-                elif request.op == "ns":
-                    try:
+                    elif request.op == "ns":
                         if request.name == "":
                             pass
                         elif request.name == "__main__":
@@ -37,10 +34,10 @@ def run():
                         else:
                             namespace = import_module(request.name)
                         write_packet(self.wfile, SN(ns=namespace.__name__))
-                    except Exception as ex:
-                        write_packet(self.wfile, SN(ex=str(ex)))
-                else:
-                    raise RuntimeError(f"Unknown request: {request}")
+                    else:
+                        raise RuntimeError(f"Unknown request: {request}")
+                except Exception as ex:
+                    write_packet(self.wfile, SN(ex=str(ex)))
 
     with ThreadingTCPServer(("localhost", 0), RequestHandler) as server:
         port = server.socket.getsockname()[1]
@@ -52,14 +49,11 @@ def run():
         finally:
             PORT_FILE_PATH.unlink()
 
-def evaluate(code, namespace):
-    try:
-        parsed = ast.parse(code)
-        statements = parsed.body
-        if len(statements) == 1 and isinstance(statements[0], ast.Expr):
-            return eval(code, namespace), None
-        else:
-            exec(code, namespace)
-            return None, None
-    except Exception as ex:
-        return None, ex
+def _evaluate(code, namespace):
+    parsed = ast.parse(code)
+    statements = parsed.body
+    if len(statements) == 1 and isinstance(statements[0], ast.Expr):
+        return eval(code, namespace)
+    else:
+        exec(code, namespace)
+        return None
